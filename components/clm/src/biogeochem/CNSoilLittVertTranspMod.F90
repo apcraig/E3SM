7bbe11ba1b90ebeb03952716088e2ba600f961cf
omearata@ornl.gov <omearata@ornl.gov>
2018-11-15 13:46:39 -0500

    updates to isolate HUM_HOL and correct water balance errors

--- components/clm/src/biogeochem/CNSoilLittVertTranspMod.F90
+++ components/clm/src/biogeochem/CNSoilLittVertTranspMod.F90
@@ -66,8 +66,10 @@ contains
      if ( .not. readv ) call endrun(msg=trim(errCode)//trim(tString)//errMsg(__FILE__, __LINE__))
      !CNSoilLittVertTranspParamsInst%som_diffus=tempr
      ! FIX(SPM,032414) - can't be pulled out since division makes things not bfb
-     CNSoilLittVertTranspParamsInst%som_diffus = 5e-4_r8 / (secspday * 365._r8)  
-
+     CNSoilLittVertTranspParamsInst%som_diffus = 1e-4_r8 / (secspday * 365._r8)  
+#if (defined HUM_HOL)
+     CNSoilLittVertTranspParamsInst%som_diffus = 5e-4_r8 / (secspday * 365._r8)
+#endif
      tString='cryoturb_diffusion_k'
      call ncd_io(trim(tString),tempr, 'read', ncid, readvar=readv)
      if ( .not. readv ) call endrun(msg=trim(errCode)//trim(tString)//errMsg(__FILE__, __LINE__))
